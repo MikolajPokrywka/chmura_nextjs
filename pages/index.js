edf409d1aed8fa3fad947a23c07824b2967aa008
Mikołaj Pokrywka <mp7961@gmail.com>
2022-10-31 14:41:56 +0100

    reapired timezone

---
+++ pages/index.js
@@ -0,0 +1,33 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+
+  function toIsoString(date) {
+    var tzo = -date.getTimezoneOffset(),
+        dif = tzo >= 0 ? '+' : '-',
+        pad = function(num) {
+            return (num < 10 ? '0' : '') + num;
+        };
+
+    return date.getFullYear() +
+        '-' + pad(date.getMonth() + 1) +
+        '-' + pad(date.getDate()) +
+        'T' + pad(date.getHours()) +
+        ':' + pad(date.getMinutes()) +
+        ':' + pad(date.getSeconds()) +
+        dif + pad(Math.floor(Math.abs(tzo) / 60)) +
+        ':' + pad(Math.abs(tzo) % 60);
+  }
+
+  var dt = new Date();
+
+
+
+  return (
+    <div >
+      {toIsoString(dt)}
+    </div>
+  )
+}
