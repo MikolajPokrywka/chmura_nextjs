85015141b75bcc34bab86dd0d2442f675ecc6a9a
Mikołaj Pokrywka <mp7961@gmail.com>
2022-10-31 19:45:06 +0100

    reapired timezone

--- pages/index.js
+++ pages/index.js
@@ -2,32 +2,45 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
-
-  function toIsoString(date) {
-    var tzo = -date.getTimezoneOffset(),
-        dif = tzo >= 0 ? '+' : '-',
-        pad = function(num) {
-            return (num < 10 ? '0' : '') + num;
-        };
-
-    return date.getFullYear() +
-        '-' + pad(date.getMonth() + 1) +
-        '-' + pad(date.getDate()) +
-        'T' + pad(date.getHours()) +
-        ':' + pad(date.getMinutes()) +
-        ':' + pad(date.getSeconds()) +
-        dif + pad(Math.floor(Math.abs(tzo) / 60)) +
-        ':' + pad(Math.abs(tzo) % 60);
-  }
-
-  var dt = new Date();
-
-
-
+export default function Home({date}) {
   return (
     <div >
-      {toIsoString(dt)}
+      {date}
     </div>
   )
 }
+
+export async function getServerSideProps(){
+
+    function toIsoString(date) {
+      var tzo = -date.getTimezoneOffset(),
+          dif = tzo >= 0 ? '+' : '-',
+          pad = function(num) {
+              return (num < 10 ? '0' : '') + num;
+          };
+  
+      return date.getFullYear() +
+          '-' + pad(date.getMonth() + 1) +
+          '-' + pad(date.getDate()) +
+          'T' + pad(date.getHours()) +
+          ':' + pad(date.getMinutes()) +
+          ':' + pad(date.getSeconds()) +
+          dif + pad(Math.floor(Math.abs(tzo) / 60)) +
+          ':' + pad(Math.abs(tzo) % 60);
+    }
+  
+    var dt = new Date();
+    return { props: { date: toIsoString(dt)} }
+
+  }
+
+// export async function getStaticPaths() {
+//   const paths = data.map(date => {
+//     return {params: {id: date}}
+//   })
+
+//   return {
+//     paths,
+//     fallback: false
+//   }
+// }
